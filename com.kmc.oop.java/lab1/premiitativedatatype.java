789beaec397321fd257974da13db7466db7ed34e
anup <aniruddharai39@gmail.com>
2025-11-27 20:24:45 +0545

    premitative datatype
    premitative datatype

--- com.kmc.oop.java/lab1/premiitativedatatype.java
+++ com.kmc.oop.java/lab1/premiitativedatatype.java
@@ -1,36 +1,10 @@
 package com.kmc.oop.java.lab1;
-public class premiitativedatatype{
-    public static void main(String[] args) {
-        // byte data type
-        byte a = 10;
-        System.out.println("Byte value: " + a);
-
-        // short data type
-        short b = 20000;
-        System.out.println("Short value: " + b);
-
-        // int data type
-        int c = 150000;
-        System.out.println("Integer value: " + c);
-
-        // long data type
-        long d = 500000L;
-        System.out.println("Long value: " + d);
-
-        // float data type
-        float e = 5.75f;
-        System.out.println("Float value: " + e);
-
-        // double data type
-        double f = 19.99d;
-        System.out.println("Double value: " + f);
-
-        // char data type
-        char g = 'A';
-        System.out.println("Character value: " + g);
-
-        // boolean data type
-        boolean h = true;
-        System.out.println("Boolean value: " + h);
+public class sumofallnumber{
+    static void main(){
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            sum=sum+i;
+        }
+        System.out.println("Sum of all numbers from 1 to 10 is: "+sum);
     }
 }
