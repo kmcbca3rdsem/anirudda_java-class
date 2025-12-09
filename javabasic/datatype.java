89442859797f68ef7734d217b207523de62ff748
anup <aniruddharai39@gmail.com>
2025-12-09 19:54:59 +0545

    basic_java
    basic-java

---
+++ javabasic/datatype.java
@@ -0,0 +1,18 @@
+package javabasic;
+
+public class datatype {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.5f;
+        char c = 'A';
+        boolean d = true;
+        String e = "Hello, World!";
+        
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + d);
+        System.out.println("String: " + e);
+    }
+}
