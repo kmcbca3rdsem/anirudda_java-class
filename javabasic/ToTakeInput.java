a0821fc34abc758ba81b81edffe53a4c0d99e073
anup <aniruddharai39@gmail.com>
2025-12-09 19:58:16 +0545

    input function
    input function

---
+++ javabasic/ToTakeInput.java
@@ -0,0 +1,16 @@
+package javabasic;
+import java.util.Scanner;
+public class ToTakeInput {
+    int age;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello-> " + name + "!");
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Your age is: " + age);
+        sc.close();
+    }
+    
+}
