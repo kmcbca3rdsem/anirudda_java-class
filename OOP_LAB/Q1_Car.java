68ba0fb0d6705167b18ecd47019f17b20d4a7ed9
anup <aniruddharai39@gmail.com>
2025-12-21 14:00:46 +0545

    lab 3
    lab 3

---
+++ OOP_LAB/Q1_Car.java
@@ -0,0 +1,22 @@
+package OOP_LAB;
+
+class Car {
+    String brand;
+    String model;
+    int year;
+
+    void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.brand = "Toyota";
+        car.model = "Corolla";
+        car.year = 2022;
+
+        car.displayDetails();
+    }
+}
