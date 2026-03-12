e56f94b94b21f5ea153c39ed8d5e29550b001d4e
anup <aniruddharai39@gmail.com>
2026-03-12 07:35:53 +0545

    hu
    hu

---
+++ lab2/rectangledemo.java
@@ -0,0 +1,24 @@
+package lab2;
+
+
+
+class Person{
+    String name;
+    int age;
+
+    void greet(){
+        System.out.println("Hello, " +name+".");
+        System.out.println("A "+age+" years old guy.");
+    }
+}
+
+public class rectangledemo{
+    public static void main(String[] args){
+        Person p1=new Person();
+
+        p1.name="Rohit";
+        p1.age=20;
+
+        p1.greet();
+    }
+}
