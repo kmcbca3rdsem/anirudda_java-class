c715789cfc479671478294d796b22f672b3fcfbf
anup <aniruddharai39@gmail.com>
2026-04-03 17:19:08 +0545

    commit
    commit

---
+++ lab5/main.java
@@ -0,0 +1,13 @@
+package lab5;
+public class main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    if (a > b && a > c) {
+        System.out.println("a is the largest number");
+    } else if (b > a && b > c) {
+        System.out.println("b is the largest number");
+    } else {
+        System.out.println("c is the largest number");
+    }
+}
