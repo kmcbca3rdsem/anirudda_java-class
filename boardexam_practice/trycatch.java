a9c993d42b30f4a7f62ec551997e9fa1be6f0ac1
anup <aniruddharai39@gmail.com>
2026-05-05 01:25:27 +0545

    jhgjk
    uhgilu

---
+++ boardexam_practice/trycatch.java
@@ -0,0 +1,24 @@
+package boardexam_practice;
+
+class student{
+    int roll,marks;
+    String name;
+    void input(){
+        System.out.println("enter the roll and name:");
+    }
+}
+class ankit extends student{
+    void disp(){
+        roll = 1;
+        name =  "ani";marks = 67;
+        System.out.println(roll+""+name+""+marks);
+    }
+
+}
+class main{
+    public static void main(String[] args){
+        ankit r = new ankit();
+        r.input();
+        r.disp();
+    }
+}
