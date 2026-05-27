a0ea7b18049ef6832a065aa70e4c5110b8f365fc
anup <aniruddharai39@gmail.com>
2026-05-27 19:34:50 +0545

    sdf
    sdf

--- Assignment/Lab4/WrapperClassDemo.java
+++ Assignment/Lab4/WrapperClassDemo.java
@@ -2,7 +2,7 @@ package Assignment.Lab4;
 public class WrapperClassDemo {
     public static void main(String[] args) {
         int num = 42;
-        Integer integerNum = Integer.valueOf(num);  // or Integer num = num; (Autoboxing)
+        Integer integerNum = Integer.valueOf(num); 
         System.out.println("Integer value: " + integerNum);
 
         int unboxedNum = integerNum.intValue();
